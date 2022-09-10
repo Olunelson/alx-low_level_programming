8410092ee79b31a7faad098b6706bb9e3f7330e3
Olunelson <oluwarotiminelson@gmail.com>
2022-09-09 22:47:05 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,11 +6,8 @@
 
 /**
  *  * main - main block
- *
  *   * Description: Get a random number and print the number
- *
  *    * and if it is positive, negative, or zero
- *
  *     * Return: 0
  */
 
