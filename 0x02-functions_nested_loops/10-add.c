2f79d79979671cd005bd4ea18df59c85a6402fcd
Olunelson <oluwarotiminelson@gmail.com>
2022-09-15 01:46:26 -0700

    addition of two numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,11 @@
 
 /**
  * add - Take two parameters to add two integers
- * @x: int type number 1
- * @y: int type number 2
+ * @a: int type number 1
+ * @b: int type number 2
  * Return: return the addition
  */
-int add(int x, int y)
+int add(int a, int b)
 {
-	return (x + y);
+	return (a + b);
 }
