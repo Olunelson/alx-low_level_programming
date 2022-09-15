596c81d927c9d8e0eb6168bed83b48b3134d756e
Olunelson <oluwarotiminelson@gmail.com>
2022-09-15 01:40:17 -0700

    addition of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - Take two parameters to add two integers
+ * @x: int type number 1
+ * @y: int type number 2
+ * Return: return the addition
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
