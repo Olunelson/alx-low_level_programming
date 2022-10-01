b24b4136fbb14ce1267b39848f728cac7d92dc89
Olunelson <oluwarotiminelson@gmail.com>
2022-09-30 18:36:59 -0700

    static-lib

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - will return 1 if letter is a lowercase & 0 if not.
+ *@c: character from 97 to 122 represent a-z
+ *Return: return 1 if lowerecase else 0
+*/
+int _islower(int c)
+{
+		if (c >= 97 && c <= 122)
+		{
+		return (1);
+		}
+		else
+		{
+		return (0);
+		}
+
+}
