9bf3e91c0c05f08d842792c156ee3b5c96beff6e
Olunelson <oluwarotiminelson@gmail.com>
2022-10-04 10:16:41 -0700

    ammend

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,10 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
-#include <stdio.h>
 
 int _putchar(char c);
 char *create_array(unsgned int size, char c);
 char *_strdup(char *str);
+
 #endif
